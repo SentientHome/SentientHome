c71b2e75815ba7f091f72677c67329e90c9bf5cb
Oliver Ratzesberger <oliver@ratzesberger.com>
2014-11-29 21:03:45 -0800

    Initial NEST data dump
    Initial NEST skeleton for dumping the NEST data. Primarily for
    investigation of data structures.

--- feed.influx.home.nest.py
+++ feed.influx.home.nest.py
@@ -20,28 +20,61 @@ influx_port    = config.get('influxdb', 'influx_port')
 influx_db      = config.get('influxdb', 'influx_db')
 influx_user    = config.get('influxdb', 'influx_user')
 influx_pass    = config.get('influxdb', 'influx_pass')
-nest_client_id         = config.get('nest', 'nest_client_id')
-nest_client_secret     = config.get('nest', 'nest_client_secret')
-nest_client_pin        = config.get('nest', 'nest_client_pin')
-nest_poll_interval     = int(config.get('nest', 'nest_poll_interval', 60))
+nest_user      = config.get('nest', 'nest_user')
+nest_pass      = config.get('nest', 'nest_pass')
+nest_cache     = config.get('nest', 'nest_cache')
+nest_poll_interval = int(config.get('nest', 'nest_poll_interval', 60))
 
 influx_path = "http://" + influx_addr + ":" + influx_port + "/db/" + influx_db + "/series?time_precision=s&u=" + influx_user
 print "Path: " + influx_path
 
-#c = Client(token_endpoint="https://api.home.nest.com/oauth2/access_token",
-#    client_id = nest_client_id,
-#    client_secret = nest_client_secret)
-
-#c.request_token(code = nest_client_pin)
-
-nest = nest_thermostat.Nest(USERNAME, PASSWORD)
+nest = nest_thermostat.Nest(nest_user, nest_pass, access_token_cache_file=os.path.expanduser(nest_cache))
 
 structures = nest.structures
 
-while True:
-#  data = c.request('wss://developer-api.nest.com')
+for structure in structures:
+  print '==========================='
+  print 'Structure: ' + structure.name
+  print 'Away     : ' + str(structure.away)
+#  print 'Location : ' + structure.location
+#  print 'Address  : ' + structure.address
+  print 'ZIP      : ' + structure.postal_code
 
-#  print data
+devices = nest.devices
+
+for device in devices:
+  print '---------------------------'
+  print 'Device   : ' + device.name
+  print 'Serial   : ' + device._serial
+  print 'Temp     : ' + str(device.temperature)
+  print 'Humidity : ' + str(device.humidity)
+  print 'Fan      : ' + str(device.fan)
+  print 'Mode     : ' + device.mode
+  print 'Target   : ' + str(device.target)
+  print 'ZIP      : ' + device.postal_code
+
+
+for key in nest._status.keys():
+  print '============================'
+  print 'Key    : ' + key
+  print 'Subkeys: ' + str(nest._status[key].keys())
+
+  for subkey in nest._status[key].keys():
+    print '----------------------------'
+    print 'SubKey  : ' + subkey + ' = ' + str(nest._status[key][subkey])
+    print 'Sub2keys: ' + str(nest._status[key][subkey].keys())
+
+    for sub2key in nest._status[key][subkey].keys():
+      print '++++++++++++++++++++++++++++'
+      print 'Sub2Key  : ' + sub2key + ' = ' + str(nest._status[key][subkey][sub2key])
+      try:
+        print 'Sub3keys: ' + str(nest._status[key][subkey][sub2key].keys())
+      except Exception:
+        continue
+
+
+while True:
+
 
   event = [{
     'name': 'nest',
