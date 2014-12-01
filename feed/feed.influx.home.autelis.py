b9c1e0591c4c6c11de2ba760cd3b2fe7075fcbfc
Oliver Ratzesberger <oliver@ratzesberger.com>
2014-11-30 19:49:21 -0800

    Refactored Autelis feeder
    Basic refactored Autelis feeder leveraging the new shConfig class and
    the shared shutil module

--- feed/feed.influx.home.autelis.py
+++ feed/feed.influx.home.autelis.py
@@ -1,71 +1,40 @@
 #!/usr/local/bin/python -u
-__author__ = "Oliver Ratzesberger"
+__author__    = 'Oliver Ratzesberger <https://github.com/fxstein>'
+__copyright__ = 'Copyright (C) 2014 Oliver Ratzesberger'
+__license__   = 'Apache License, Version 2.0'
 
-print "Starting InfluxDB JSON feed for Autelis PentAir Easytouch Controller"
+# Make sure we have access to SentientHome commons
+import os, sys
+sys.path.append(os.path.abspath('..'))
+
+# Sentient Home configuration
+from common.shconfig import shConfig
+from common.shutil import etree_to_dict
+
+import logging as log
+log.info('Starting feed for Autelis PentAir Easytouch Controller')
 
 import requests
 import json
 import xml.etree.ElementTree as ET
-from   collections import defaultdict
 import time
-import os
-import ConfigParser
 
-# Helper function needed to convert part of the XML response to a Dictionary
-def etree_to_dict(t):
-  d = {t.tag: {} if t.attrib else None }
-
-  children = list(t)
-  if children:
-      dd = defaultdict(list)
-      for dc in map(etree_to_dict, children):
-          for k, v in dc.iteritems():
-              dd[k].append(v)
-      d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
-  if t.attrib:
-      d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
-  if t.text:
-      text = t.text.strip()
-      if children or t.attrib:
-          if text:
-            d[t.tag]['#text'] = text
-      else:
-        # When asigning the value try integer, then float, then text
-        try:
-          d[t.tag] = int(text)
-        except ValueError:
-          try:
-            d[t.tag] = float(text)
-          except ValueError:
-            d[t.tag] = text
-  return d
-
-config = ConfigParser.ConfigParser()
-config.read(os.path.expanduser('~/.config/home/home.cfg'))
-
-autelis_addr   = config.get('autelis', 'autelis_addr')
-autelis_user   = config.get('autelis', 'autelis_user')
-autelis_pass   = config.get('autelis', 'autelis_pass')
-autelis_poll_interval = int(config.get('autelis', 'autelis_poll_interval', 1))
-influx_addr    = config.get('influxdb', 'influx_addr')
-influx_port    = config.get('influxdb', 'influx_port')
-influx_db      = config.get('influxdb', 'influx_db')
-influx_user    = config.get('influxdb', 'influx_user')
-influx_pass    = config.get('influxdb', 'influx_pass')
-
-influx_path = "http://" + influx_addr + ":" + influx_port + "/db/" + influx_db + "/series?time_precision=s&u=" + influx_user
-print "Path: " + influx_path
+config = shConfig('~/.config/home/home.cfg')
 
 while True:
-  status = requests.get('http://' + autelis_addr + '/status.xml', auth=(autelis_user, autelis_pass))
+  response = requests.get('http://' + config.get('autelis', 'autelis_addr') +\
+                        '/status.xml', auth=(config.get('autelis', 'autelis_user'),\
+                         config.get('autelis', 'autelis_pass')))
 
   # For offline development:
   #data = etree_to_dict(ET.parse('samples/autelis.status.xml').getroot())
 
-  data = etree_to_dict(ET.fromstring(status.text))
+  data = etree_to_dict(ET.fromstring(response.text))
   # Data Structure Documentation: http://www.autelis.com/wiki/index.php?title=Pool_Control_(PI)_HTTP_Command_Reference
 
-  alldata = dict(data['response']['equipment'].items() + data['response']['system'].items() + data['response']['temp'].items())
+  alldata = dict(data['response']['equipment'].items() + \
+                data['response']['system'].items() + \
+                data['response']['temp'].items())
 
   event = [{
     'name': 'pool',
@@ -73,13 +42,13 @@ while True:
     'points': [ alldata.values() ]
   }]
 
-  print event
+  log.debug('Event data: %s', event)
 
   try:
-    r = requests.post(influx_path + "&p=" + influx_pass, data=json.dumps(event))
-    print r
+    r = requests.post(config.getTargetPath(), data=json.dumps(event))
+    log.info(r)
   except Exception:
-    print 'Exception posting data to ' + influx_path
+    log.warn('Exception posting data to %s', config.getTargetPathSafe())
     pass
 
-  time.sleep(autelis_poll_interval)
+  time.sleep(config.getint('autelis', 'autelis_poll_interval', 5))
