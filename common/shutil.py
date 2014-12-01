02536532ede89fc86d98665702e298ef2a7a9fd7
Oliver Ratzesberger <oliver@ratzesberger.com>
2014-12-01 02:24:00 -0800

    Add text_etree_to_dict
    Add text_etree_to_dict and cleanup indents

--- common/shutil.py
+++ common/shutil.py
@@ -4,35 +4,41 @@ __copyright__ = 'Copyright (C) 2014 Oliver Ratzesberger'
 __license__   = 'Apache License, Version 2.0'
 
 from collections import defaultdict
+import xml.etree.ElementTree as ET
+
+def text_etree_to_dict(text):
+    t = ET.fromstring(text)
+
+    return etree_to_dict(t)
 
 # Helper function needed to convert an XML response to a Dictionary
 def etree_to_dict(t):
-  d = {t.tag: {} if t.attrib else None }
+    d = {t.tag: {} if t.attrib else None }
 
-  c = list(t)
-  if c:
-      dd = defaultdict(list)
-      for dc in map(etree_to_dict, c):
-          for k, v in dc.iteritems():
-              dd[k].append(v)
-      d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
-  if t.attrib:
-      d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
-  if t.text:
-      text = t.text.strip()
-      if c or t.attrib:
-          if text:
-            d[t.tag]['#text'] = text
-      else:
-        # When asigning the value try integer, then float, then text
-        try:
-          d[t.tag] = int(text)
-        except ValueError:
-          try:
-            d[t.tag] = float(text)
-          except ValueError:
-            d[t.tag] = text
-  return d
+    c = list(t)
+    if c:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, c):
+            for k, v in dc.iteritems():
+                dd[k].append(v)
+        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
+    if t.text:
+        text = t.text.strip()
+        if c or t.attrib:
+            if text:
+                d[t.tag]['#text'] = text
+        else:
+            # When asigning the value try integer, then float, then text
+            try:
+                d[t.tag] = int(text)
+            except ValueError:
+                try:
+                    d[t.tag] = float(text)
+                except ValueError:
+                    d[t.tag] = text
+    return d
 
 #
 # Do nothing
