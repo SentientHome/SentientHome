10927b93fd02d82f39faa9ac061f38b6147f95f7
Oliver Ratzesberger <oratzes@searshc.com>
2015-01-30 07:32:49 -0800

    Restructure project tree

--- SentientHome/feed/feed.home.netatmo.py
+++ SentientHome/feed/feed.home.netatmo.py
@@ -5,7 +5,7 @@ __license__   = 'Apache License, Version 2.0'
 
 # Make sure we have access to SentientHome commons
 import os, sys
-sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.dirname(os.path.abspath(__file__))  + '/..')
 
 # Sentient Home configuration
 from common.shconfig import shConfig
