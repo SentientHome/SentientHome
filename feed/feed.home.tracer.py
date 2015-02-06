389ef5a9cdab88209b804a9e60fcbdfe5d58f5c5
fxstein <oliver@ratzesberger.com>
2015-02-06 14:09:13 -0800

    Add simple tracer events

---
+++ feed/feed.home.tracer.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3 -u
+__author__    = 'Oliver Ratzesberger <https://github.com/fxstein>'
+__copyright__ = 'Copyright (C) 2015 Oliver Ratzesberger'
+__license__   = 'Apache License, Version 2.0'
+
+# Make sure we have access to SentientHome commons
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__))  + '/..')
+
+# Sentient Home configuration
+from common.shconfig import shConfig
+from common.sheventhandler import shEventHandler
+
+import logging as log
+log.info('Starting SentientHome Tracer')
+
+import time
+
+# Simple tracer that 'fires' events on a predefined interval
+
+config = shConfig('~/.config/home/home.cfg')
+handler = shEventHandler(config, config.getfloat('sentienthome', 'tracer_interval', 10))
+
+count = 0
+
+while True:
+    count += 1
+
+    event = [{
+        'name': 'tracer', # Time Series Name
+        'columns': ['time', 'count'], # Keys
+        'points': [[time.time(), count]] # Data points
+    }]
+
+    log.debug('Event data: %s', event)
+
+    handler.postEvent(event)
+
+    # We reset the poll interval in case the configuration has changed
+    handler.sleep(config.getfloat('sentienthome', 'tracer_interval', 10))
