8bdadc47e232ca1ccaf456cc0ee0e63723c16448
fxstein <oliver@ratzesberger.com>
2015-02-07 19:42:00 -0800

    Python 3 fixes

--- setup.py
+++ setup.py
@@ -6,32 +6,32 @@ __license__   = 'Apache License, Version 2.0'
 # Make sure we have access to SentientHome commons
 import os
 
-print 'Checking node.js presence:'
+print('Checking node.js presence:')
 if 0 != os.system('node -v'):
-    print 'node.js not present on system. Exiting...'
+    print('node.js not present on system. Exiting...')
     quit()
 
-print 'Installing node.js dependencies:'
+print('Installing node.js dependencies:')
 if 0 != os.system('npm install home'):
-    print 'Error installing node.js home module. Exiting...'
+    print('Error installing node.js home module. Exiting...')
     quit()
 
 if 0 != os.system('npm install iniparser'):
-    print 'Error installing node.js iniparser module. Exiting...'
+    print('Error installing node.js iniparser module. Exiting...')
     quit()
 
-print 'Installing Python3 dependencies'
+print('Installing Python3 dependencies')
 
 if 0 != os.system('pip3 install requests'):
-    print 'Error installing python3 requests package. Exiting...'
+    print('Error installing python3 requests package. Exiting...')
     quit()
 
 if 0 != os.system('pip3 install asyncio'):
-    print 'Error installing python3 asyncio package. Exiting...'
+    print('Error installing python3 asyncio package. Exiting...')
     quit()
 
 if 0 != os.system('pip3 install aiohttp'):
-    print 'Error installing python3 aiohttp package. Exiting...'
+    print('Error installing python3 aiohttp package. Exiting...')
     quit()
 
-print 'Finished installing dependencies.'
+print('Finished installing dependencies.')
