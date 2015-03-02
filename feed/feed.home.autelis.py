c85f78e2cd1d4c1a97c64d96523149c711eda026
fxstein <oliver@ratzesberger.com>
2015-03-01 17:35:19 -0800

    Use shRegistry and add exception handling
    Leverage shRegistry to retrieve time series name and add additional
    error handling

--- feed/feed.home.autelis.py
+++ feed/feed.home.autelis.py
@@ -11,6 +11,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))  + '/..')
 from common.shconfig import shConfig
 from common.shutil import xml_to_dict
 from common.sheventhandler import shEventHandler
+from common.shregistry import shRegistry
 
 import logging as log
 
@@ -20,29 +21,33 @@ handler = shEventHandler(config,\
                          config.getfloat('autelis', 'autelis_poll_interval', 10))
 
 while True:
-    r = handler.get('http://' + config.get('autelis', 'autelis_addr') +\
-                         '/status.xml', auth=(config.get('autelis', 'autelis_user'),\
-                         config.get('autelis', 'autelis_pass')))
-        # Data Structure Documentation: http://www.autelis.com/wiki/index.php?
-        # title=Pool_Control_(PI)_HTTP_Command_Reference
+    try:
+        r = handler.get('http://' + config.get('autelis', 'autelis_addr') +\
+                             '/status.xml', auth=(config.get('autelis', 'autelis_user'),\
+                             config.get('autelis', 'autelis_pass')))
+            # Data Structure Documentation: http://www.autelis.com/wiki/index.php?
+            # title=Pool_Control_(PI)_HTTP_Command_Reference
 
-    log.debug('Fetch data: %s', r.text)
+        log.debug('Fetch data: %s', r.text)
 
-    data = xml_to_dict(r.text)
+        data = xml_to_dict(r.text)
 
-    alldata = dict(list(data['response']['equipment'].items()) + \
-                    list(data['response']['system'].items()) + \
-                    list(data['response']['temp'].items()))
+        alldata = dict(list(data['response']['equipment'].items()) + \
+                       list(data['response']['system'].items()) + \
+                       list(data['response']['temp'].items()))
 
-    event = [{
-        'name': 'pool', # Time Series Name
-        'columns': list(alldata.keys()), # Keys
-        'points': [ list(alldata.values()) ] # Data points
-    }]
+        event = [{
+            'name':    shRegistry['autelis']['name'], # Time Series Name
+            'columns': list(alldata.keys()), # Keys
+            'points':  [ list(alldata.values()) ] # Data points
+        }]
 
-    log.debug('Event data: %s', event)
+        log.debug('Event data: %s', event)
 
-    handler.postEvent(event)
+        handler.postEvent(event)
+    except Exception as e:
+        log.error('Error: %s', e)
+        pass
 
     # We reset the poll interval in case the configuration has changed
     handler.sleep(config.getfloat('autelis', 'autelis_poll_interval', 10))
