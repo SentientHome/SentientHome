e50da3e397d53c13e4d18b5efc86fae43a65821c
fxstein <oliver@ratzesberger.com>
2015-03-21 08:48:41 -0700

    Cleanup mFi Sensor logic
    Cleanup mFi Sensor logic and inline comments

--- feed/feed.home.ubnt.mfi.py
+++ feed/feed.home.ubnt.mfi.py
@@ -19,27 +19,31 @@ import json
 config = shConfig('~/.config/home/home.cfg', name='Ubiquiti Unifi mFi Data')
 
 # default to 5 sec polls - the frequency most sensors report back to the controller
-handler = shEventHandler(config, config.getfloat('ubnt_mfi', 'ubnt_mfi_poll_interval', 5), dedupe=True)
+handler = shEventHandler(config,\
+                         config.getfloat('ubnt_mfi', 'ubnt_mfi_poll_interval', 5),\
+                         dedupe=True)
 
 retries = 0
 
 # Setup session and login
 session = requests.session()
-addr = config.get('ubnt_mfi', 'ubnt_mfi_addr')
-r = session.post('https://' + addr + '/login',\
-                    {'username': config.get('ubnt_mfi', 'ubnt_mfi_user'),\
-                     'password': config.get('ubnt_mfi', 'ubnt_mfi_pass'),\
-                     'login':'Login'},\
-                      verify=config.getboolean('ubnt_mfi', 'ubnt_mfi_verify_ssl'))
+r = session.post('https://' + config.get('ubnt_mfi', 'ubnt_mfi_addr') +\
+                  '/login',\
+                  {'username': config.get('ubnt_mfi', 'ubnt_mfi_user'),\
+                   'password': config.get('ubnt_mfi', 'ubnt_mfi_pass'),\
+                   'login':'Login'},\
+                  verify=config.getboolean('ubnt_mfi', 'ubnt_mfi_verify_ssl'))
 
 while True:
-    # Get all earthquakes of the past hour
-    r = session.get('https://' + addr + '/api/v1.0/list/sensors',\
+    # Get all sensors and their most current data
+    # We can do this 10x per second if needed to drive down latency
+    # This is just the polling part, we then auto dedupe the data to only
+    # emit changed sensor values to the engine and event store
+    r = session.get('https://' + config.get('ubnt_mfi', 'ubnt_mfi_addr') +\
+                    '/api/v1.0/list/sensors',\
                     verify=config.getboolean('ubnt_mfi', 'ubnt_mfi_verify_ssl'))
 
     data = json.loads(r.text)
-
-    log.debug('Data: %s', data)
 
     for sensor in data['data']:
         event = [{
@@ -50,10 +54,10 @@ while True:
 
         log.debug('Event: %s', event)
         # dedupe automatically ignores events we have processed before
+        # This is where the dedupe magic happens. The event handler has deduping
+        # built in and keeps an in-memory cache of events of the past 24h for that
+        # In this case only changed sensor data points will get emitted and stored
         handler.postEvent(event, dedupe=True)
 
     # We reset the poll interval in case the configuration has changed
     handler.sleep(config.getfloat('ubnt_mfi', 'ubnt_mfi_poll_interval', 5))
-
-    # Reset poll path before looping with latest setting
-    addr = config.get('ubnt_mfi', 'ubnt_mfi_addr')
