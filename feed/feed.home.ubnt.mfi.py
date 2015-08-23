32527c558e14120409081c61c7c831ca00bd90fa
fxstein <oliver@ratzesberger.com>
2015-08-22 22:07:04 -0700

    Initial port of ubnt.mfi feeder to Cement framework

--- feed/feed.home.ubnt.mfi.py
+++ feed/feed.home.ubnt.mfi.py
@@ -7,48 +7,84 @@ __license__   = 'Apache License, Version 2.0'
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__))  + '/..')
 
-# Sentient Home configuration
-from common.shconfig import shConfig
+# Sentient Home Application
+from common.shapp import shApp
 from common.sheventhandler import shEventHandler
 from common.shregistry import shRegistry
 from common.shutil import CtoF
 
-import logging as log
 import requests
 import json
 
-config = shConfig('~/.config/home/home.cfg', name='Ubiquiti Unifi mFi Data')
+# Default settings
+from cement.utils.misc import init_defaults
 
-# default to 5 sec polls - the frequency most sensors report back to the controller
-handler = shEventHandler(config,\
-                         config.getfloat('ubnt_mfi', 'ubnt_mfi_poll_interval', 5),\
-                         dedupe=True)
+defaults = init_defaults('ubnt_mfi', 'ubnt_mfi')
+defaults['ubnt_mfi']['poll_interval'] = 5.0
 
-retries = 0
+with shApp('ubnt_mfi', config_defaults=defaults) as app:
+    app.run()
 
-# Setup session and login
-session = requests.session()
-r = session.post('https://' + config.get('ubnt_mfi', 'ubnt_mfi_addr') +\
-                  '/login',\
-                  {'username': config.get('ubnt_mfi', 'ubnt_mfi_user'),\
-                   'password': config.get('ubnt_mfi', 'ubnt_mfi_pass'),\
-                   'login':'Login'},\
-                  verify=config.getboolean('ubnt_mfi', 'ubnt_mfi_verify_ssl'))
+    handler = shEventHandler(app, dedupe=True)
 
-while True:
-    try:
+    retries = 0
+
+    # Setup session and login
+    session = requests.session()
+
+    while True:
+        try:
+            r = session.post(app.config.get('ubnt_mfi', 'ubnt_mfi_addr') + ':' +\
+                        app.config.get('ubnt_mfi', 'ubnt_mfi_port') + '/login',\
+                      {'username': app.config.get('ubnt_mfi', 'ubnt_mfi_user'),\
+                       'password': app.config.get('ubnt_mfi', 'ubnt_mfi_pass'),\
+                       'login':'Login'},\
+                        verify=(int)(app.config.get('ubnt_mfi', 'ubnt_mfi_verify_ssl')))
+            break
+        except Exception as e:
+            retries += 1
+
+            # Something went wrong authorizing the connection to the ubnt mfi service
+            app.log.warn(e)
+            app.log.warn('Cannot connect to MFI. Attemp %s of %s' % (retries, app.retries))
+
+            if retries >= app.retries:
+                app.log.fatal(e)
+                app.log.fatal('Unable to connect to MFI. Exiting...')
+                app.close(1)
+
+            handler.sleep(app.retry_interval)
+
+    while True:
         # Get all sensors and their most current data
-        # We can do this 10x per second if needed to drive down latency
+        # We could do this multiple times per second if needed to drive down latency
         # This is just the polling part, we then auto dedupe the data to only
         # emit changed sensor values to the engine and event store
-        r = session.get('https://' + config.get('ubnt_mfi', 'ubnt_mfi_addr') +\
-                        '/api/v1.0/list/sensors',\
-                        verify=config.getboolean('ubnt_mfi', 'ubnt_mfi_verify_ssl'))
+        retries = 0
+
+        while True:
+            try:
+                r = session.get(app.config.get('ubnt_mfi', 'ubnt_mfi_addr') + ':' +\
+                                app.config.get('ubnt_mfi', 'ubnt_mfi_port') + '/api/v1.0/list/sensors',\
+                                verify=(int)(app.config.get('ubnt_mfi', 'ubnt_mfi_verify_ssl')))
+                break
+            except Exception as e:
+                retries += 1
+
+                # Something went wrong connecting to the ubnt mfi service
+                app.log.warn(e)
+                app.log.warn('Cannot connect to MFI. Attemp %s of %s' % (retries, app.retries))
+
+                if retries >= app.retries:
+                    app.log.fatal(e)
+                    app.log.fatal('Unable to connect to MFI. Exiting...')
+                    app.close(1)
+
+                handler.sleep(app.retry_interval)
 
         data = json.loads(r.text)
 
         for sensor in data['data']:
-
             # If there is a temerature reading also create a Farenheit version
             # We have to do this since InfluxDB and/or Grafana cant do the most
             # simplistic math...
@@ -63,15 +99,12 @@ while True:
                 'points': [list(sensor.values())] # Data points
             }]
 
-            log.debug('Event: %s', event)
+            app.log.debug('Event: %s' % event)
             # dedupe automatically ignores events we have processed before
             # This is where the dedupe magic happens. The event handler has deduping
             # built in and keeps an in-memory cache of events of the past 24h for that
             # In this case only changed sensor data points will get emitted and stored
             handler.postEvent(event, dedupe=True)
-    except Exception as e:
-        log.error('Error: %s', e)
-        pass
 
-    # We reset the poll interval in case the configuration has changed
-    handler.sleep(config.getfloat('ubnt_mfi', 'ubnt_mfi_poll_interval', 5))
+        # We reset the poll interval in case the configuration has changed
+        handler.sleep()
