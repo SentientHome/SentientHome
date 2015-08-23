0116271539058ecee0b27f1e37bf953d6ea58e9c
fxstein <oliver@ratzesberger.com>
2015-08-22 22:18:08 -0700

    Initial port of usgs.earthquake feed to Cement framework

--- feed/feed.home.usgs.earthquake.py
+++ feed/feed.home.usgs.earthquake.py
@@ -7,89 +7,93 @@ __license__   = 'Apache License, Version 2.0'
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__))  + '/..')
 
-# Sentient Home configuration
-from common.shconfig import shConfig
+# Sentient Home Application
+from common.shapp import shApp
 from common.sheventhandler import shEventHandler
 
-import logging as log
 import requests
 import json
 
-config = shConfig('~/.config/home/home.cfg', name='USGS Earthquake Data')
-# default to 5 min polls - the frequency the USGS update the feed
-handler = shEventHandler(config, config.getfloat('usgs_quake', 'usgs_quake_poll_interval', 300), dedupe=True)
+# Default settings
+from cement.utils.misc import init_defaults
 
-retries = 0
+defaults = init_defaults('usgs_quake', 'usgs_quake')
+defaults['usgs_quake']['poll_interval'] = 10.0
 
-# Initial poll - most likely a day if the increments are hourly data
-# We do this to catchup on events in case the feed was down for some time
-path = config.get('usgs_quake', 'usgs_quake_init_path')
+with shApp('usgs_quake', config_defaults=defaults) as app:
+    app.run()
 
-while True:
-    # Get all earthquakes of the past hour
-    r = handler.get('http://' + config.get('usgs_quake', 'usgs_quake_addr') +\
-                     path)
-    data = json.loads(r.text)
+    handler = shEventHandler(app, dedupe=True)
 
-    m = data['metadata']
+    # Initial poll - most likely a day if the increments are hourly data
+    # We do this to catchup on events in case the feed was down for some time
+    path = app.config.get('usgs_quake', 'usgs_quake_init_path')
 
-    event = [{
-        'name': 'usgs.earthquake.metadata', # Time Series Name
-        'columns': ['title', 'count', 'generated', 'api', 'status'], # Keys
-        'points': [[ m['title'], m['count'], m['generated'],\
-                     m['api'], m['status'] ]] # Data points
-    }]
+    while True:
+        # Get all earthquakes of the past hour
+        r = handler.get(app.config.get('usgs_quake', 'usgs_quake_addr') +\
+                         path)
+        data = json.loads(r.text)
 
-    log.debug('Event data: %s', event)
+        m = data['metadata']
 
-    handler.postEvent(event, dedupe=True)
-
-    # Need to revser the order of incoming events as news are on top but we need
-    # to process in the order they happened.
-    features = data['features'][::-1]
-
-    for f in features:
         event = [{
-            'name': 'usgs.earthquake.feature', # Time Series Name
-            'columns': ['id', 'long', 'lat', 'depth', 'mag', 'type',
-                        'magtype', 'tz', 'felt', 'place', 'status',
-                        'gap', 'dmin', 'rms', 'ids', 'title', 'types',
-                        'cdi', 'net', 'nst', 'sources', 'alert', 'time',
-                        'tsunami', 'code', 'sig'
-                        ], # Keys
-            'points': [[ f['id'],\
-                         f['geometry']['coordinates'][0],\
-                         f['geometry']['coordinates'][1],\
-                         f['geometry']['coordinates'][2],\
-                         f['properties']['mag'],\
-                         f['properties']['type'],\
-                         f['properties']['magType'],\
-                         f['properties']['tz'],\
-                         f['properties']['felt'],\
-                         f['properties']['place'],\
-                         f['properties']['status'],\
-                         f['properties']['gap'],\
-                         f['properties']['dmin'],\
-                         f['properties']['rms'],\
-                         f['properties']['ids'],\
-                         f['properties']['title'],\
-                         f['properties']['types'],\
-                         f['properties']['cdi'],\
-                         f['properties']['net'],\
-                         f['properties']['nst'],\
-                         f['properties']['sources'],\
-                         f['properties']['alert'],\
-                         f['properties']['time'],\
-                         f['properties']['tsunami'],\
-                         f['properties']['code'],\
-                         f['properties']['sig']
-                          ]] # Data points
+            'name': 'usgs.earthquake.metadata', # Time Series Name
+            'columns': ['title', 'count', 'generated', 'api', 'status'], # Keys
+            'points': [[ m['title'], m['count'], m['generated'],\
+                         m['api'], m['status'] ]] # Data points
         }]
-        # dedupe automatically ignores events we have processed before
+
+        app.log.debug('Event data: %s' % event)
+
         handler.postEvent(event, dedupe=True)
 
-    # We reset the poll interval in case the configuration has changed
-    handler.sleep(config.getfloat('usgs_quake', 'usgs_quake_poll_interval', 300))
+        # Need to revser the order of incoming events as news are on top but we need
+        # to process in the order they happened.
+        features = data['features'][::-1]
 
-    # Reset poll path before looping with latest setting
-    path = config.get('usgs_quake', 'usgs_quake_path')
+        for f in features:
+            event = [{
+                'name': 'usgs.earthquake.feature', # Time Series Name
+                'columns': ['id', 'long', 'lat', 'depth', 'mag', 'type',
+                            'magtype', 'tz', 'felt', 'place', 'status',
+                            'gap', 'dmin', 'rms', 'ids', 'title', 'types',
+                            'cdi', 'net', 'nst', 'sources', 'alert', 'time',
+                            'tsunami', 'code', 'sig'
+                            ], # Keys
+                'points': [[ f['id'],\
+                             f['geometry']['coordinates'][0],\
+                             f['geometry']['coordinates'][1],\
+                             f['geometry']['coordinates'][2],\
+                             f['properties']['mag'],\
+                             f['properties']['type'],\
+                             f['properties']['magType'],\
+                             f['properties']['tz'],\
+                             f['properties']['felt'],\
+                             f['properties']['place'],\
+                             f['properties']['status'],\
+                             f['properties']['gap'],\
+                             f['properties']['dmin'],\
+                             f['properties']['rms'],\
+                             f['properties']['ids'],\
+                             f['properties']['title'],\
+                             f['properties']['types'],\
+                             f['properties']['cdi'],\
+                             f['properties']['net'],\
+                             f['properties']['nst'],\
+                             f['properties']['sources'],\
+                             f['properties']['alert'],\
+                             f['properties']['time'],\
+                             f['properties']['tsunami'],\
+                             f['properties']['code'],\
+                             f['properties']['sig']
+                              ]] # Data points
+            }]
+            # dedupe automatically ignores events we have processed before
+            handler.postEvent(event, dedupe=True)
+
+        # We reset the poll interval in case the configuration has changed
+        handler.sleep()
+
+        # Reset poll path before looping with latest setting
+        path = app.config.get('usgs_quake', 'usgs_quake_path')
